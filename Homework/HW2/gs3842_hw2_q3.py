a19333a9a688ff8e2037eb6c455cd296ba192d81
Gangwon Suh <117620824+gswon@users.noreply.github.com>
2023-03-05 17:28:36 -0500

    Update gs3842_hw2_q3.py

--- Homework/HW2/gs3842_hw2_q3.py
+++ Homework/HW2/gs3842_hw2_q3.py
@@ -5,4 +5,5 @@ def factors(num):
     
     for i in range(int(num**0.5) - 1, 0, -1):
         if num % i == 0:
-            yield num // i
+            if (i**2) != num: #
+                yield num // i
