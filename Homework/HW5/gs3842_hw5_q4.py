599fb5f6cb8e941dc8a0744eee6ada0af210dafa
Gangwon Suh <117620824+gswon@users.noreply.github.com>
2023-04-05 23:11:57 -0400

    Update gs3842_hw5_q4.py

--- Homework/HW5/gs3842_hw5_q4.py
+++ Homework/HW5/gs3842_hw5_q4.py
@@ -4,44 +4,56 @@ class Queue:
     def __init__(self):
         self.data1 = ArrayStack()
         self.data2 = ArrayStack()
-        # self.data2는 dequeue, first method사용할때만 쓰이고
-        # 나머지일때는 안쓰이는 방식이 맞는지여부
 
     def __len__(self):
-        return len(self.data1) # len(self) 인지
+        return (len(self.data1) + len(self.data2))
     
     def is_empty(self):
-        return len(self.data1) == 0 # len(Self.data) == 0인지 
+        return (len(self.data1) + len(self.data2)) == 0
     
     def enqueue(self, val):
         self.data1.push(val)
 
     def dequeue(self):
-        if self.data1.is_empty():
+        if self.is_empty():
             raise Exception("Queue is empty")
+        elif self.data2.is_empty():
+            while self.data1.is_empty() == False:
+                self.data2.push(self.data1.pop())
         
-        for i in range(len(self.data1)-1):
-            self.data2.push(self.data1.pop())
-
-        val = self.data1.pop()
-
-        for j in range(len(self.data2)):
-            self.data1.push(self.data2.pop())
-            
-        return val
-    # 시간복잡도는 상관없는지
+        return self.data2.pop()
     
-    def first(self): # first도 적어야하는지, 더 적어야하는 method있는지 
-        if self.data1.is_empty():
+    def first(self):
+        if self.is_empty():
             raise Exception("Queue is empty")
-        for i in range(len(self.data1)-1):
-            self.data2.push(self.data1.pop())
+        elif self.data2.is_empty():
+            for i in range(len(self.data1)):
+                self.data2.push(self.data1.pop())
 
-        val = self.data1.pop()
-        self.data1.push(val)
+        return self.data2.top()
 
-        for j in range(len(self.data2)):
-            self.data1.push(self.data2.pop())
-            
-        return val
-        
+q = Queue()
+print(q.is_empty())
+print(len(q))
+print(q.dequeue())
+
+# q = Queue()
+# print(q.is_empty()) # True
+# q.enqueue(1)
+# q.enqueue(2)
+# print(q.first()) # 1
+# q.enqueue(3)
+# q.enqueue(4)
+# print(len(q)) # 4
+# print(q.is_empty()) # false
+# print(q.dequeue()) # 1
+# print(q.dequeue()) # 2
+# print(len(q)) # 2
+# q.enqueue(100)
+# print(q.is_empty()) # false
+# print(len(q)) # 3
+# print(q.dequeue()) # 3 
+# print(q.dequeue()) # 4
+# print(q.dequeue()) # 100
+# print(len(q)) # 0
+# print(q.is_empty()) # True
