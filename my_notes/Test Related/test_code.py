ba4cc76e43b2a97c2310b580652ed8b1c5ffce05
Gangwon Suh <117620824+gswon@users.noreply.github.com>
2023-05-09 21:36:06 -0400

    Update test_code.py

--- my_notes/Test Related/test_code.py
+++ my_notes/Test Related/test_code.py
@@ -1,1 +1,15 @@
-
+# Lab12 - Q1
+    root = LinkedBinaryTree.Node(2)
+    root.left = LinkedBinaryTree.Node(7)
+    root.right = LinkedBinaryTree.Node(5)
+    root.right.left = LinkedBinaryTree.Node(10)
+    root.left.left = LinkedBinaryTree.Node(2)
+    root.left.right = LinkedBinaryTree.Node(6)
+    root.right.right = LinkedBinaryTree.Node(9)
+    root.right.right.left = LinkedBinaryTree.Node(4)
+    root.left.right.left = LinkedBinaryTree.Node(5)
+    root.left.right.right = LinkedBinaryTree.Node(11)
+    root.right.right.right = LinkedBinaryTree.Node(100)
+    root.left.left.left = LinkedBinaryTree.Node(103)
+    root.left.left.right = LinkedBinaryTree.Node(101)
+    bin_tree = LinkedBinaryTree(root)
